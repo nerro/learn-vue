753a409996e55e4b3eb897f6b6ecd0ae6429f72b
Pavel Sorokin <pavel.sorokin@nerro.eu>
2017-10-16 16:27:25 +0200

    step8: Use nested components

---
+++ 08-components_within_components/main.js
@@ -0,0 +1,22 @@
+Vue.component('task-list', {
+  template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+
+  data() {
+    return {
+      tasks: [
+        {description: 'Go to the store', complete: true},
+        {description: 'Go to the mail', complete: false},
+        {description: 'Go to the farm', complete: true},
+        {description: 'Go to work', complete: false},
+      ]
+    }
+  }
+});
+
+Vue.component('task', {
+  template: '<li><slot></slot></li>'
+});
+
+new Vue({
+  el: '#root'
+});
